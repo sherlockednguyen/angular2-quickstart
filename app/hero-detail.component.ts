11272bfda9d4da3435e31fca1e7893b61e3286d8
Dat Nguyen <dat.nguyen@dnata.com>
2016-06-13 12:17:06 +0400

    Complete journey

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Hero } from './hero';
 import { RouteParams } from '@angular/router-deprecated';
 import { HeroService } from './hero.service'
@@ -10,18 +10,39 @@ import { HeroService } from './hero.service'
 })
 export class HeroDetailComponent implements OnInit{
 	hero: Hero;
+	@Output() close = new EventEmitter();
+	navigated: boolean = false;
+	error: any;
 
 	constructor(private heroService: HeroService,
 				private routeParams: RouteParams){}
 
 	ngOnInit(){
-		let id = +this.routeParams.get('id');
-		this.heroService.getHero(id).then(
-			hero => this.hero = hero
-		);
+		if(this.routeParams.get('id') !== null){
+			let id = +this.routeParams.get('id');
+			this.navigated = true;
+			this.heroService.getHero(id).then(
+				hero => this.hero = hero
+			);
+		} else {
+			this.navigated = false;
+			this.hero = new Hero();
+		}
 	}
 
-	goBack() {
-		window.history.back();
+	save(){
+		this.heroService.save(this.hero)
+			.then(hero => {
+				this.hero = hero;
+				this.goBack(hero);
+			})
+			.catch(error => this.error = error);
+	}
+
+	goBack(savedHero: Hero = null) {
+		this.close.emit(savedHero);
+		if (this.navigated) {
+			window.history.back();
+		}
 	}
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,13 +3,17 @@ import { OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 import { Router } from '@angular/router-deprecated';
+import { HeroDetailComponent } from './hero-detail.component'
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  styleUrls: ['app/heroes.component.css']
+  styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
+	addingHero: boolean = false;
+	error: any;
 	constructor(
 		private heroService: HeroService,
 		private router: Router
@@ -28,5 +32,26 @@ export class HeroesComponent implements OnInit {
 
 	gotoDetail(){
 		this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+	}
+
+	addHero() {
+		this.addingHero = true;
+		this.selectedHero = null;
+	}
+
+	close(savedHero: Hero) {
+		this.addingHero = false;
+		if (savedHero) { this.getHeroes(); }
+	}
+
+	delete(hero: Hero, event: any) {
+		event.stopPropagation();
+		this.heroService
+			.delete(hero)
+			.then(res => {
+				this.heroes = this.heroes.filter(h => h !== hero);
+				if (this.selectedHero === hero) { this.selectedHero = null; }
+			})
+			.catch(error => this.error = error); // TODO: Display error message
 	}
 }
